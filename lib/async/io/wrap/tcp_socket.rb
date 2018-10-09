c70f372d943ac33d9118f6720bfbe079a575788b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-10-09 23:11:27 +1300

    Initial support for native wrappers with stdlib interfaces.

--- lib/async/io/wrap/tcp_socket.rb
+++ lib/async/io/wrap/tcp_socket.rb
@@ -1,4 +1,4 @@
-# Copyright, 2018, by Thibaut Girka
+# Copyright, 2018, by Samuel G. D. Williams. <http://www.codeotaku.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -18,36 +18,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'http'
-require 'openssl'
+require_relative '../tcp_socket'
 
-require 'async/io/wrap/tcp_socket'
-require 'async/io/wrap/ssl_socket'
-
-RSpec.describe Async::IO::Wrap do
-	let(:wrappers) do
-		{socket_class: Async::IO::Wrap::TCPSocket, ssl_socket_class: Async::IO::Wrap::SSLSocket}
-	end
-	
-	describe "inside reactor" do
-		include_context Async::RSpec::Reactor
-		
-		it "should fetch page" do
-			expect(Async::IO::TCPSocket).to receive(:new).and_call_original
-			
-			expect do
-				response = HTTP.get('https://www.google.com', wrappers)
-				response.connection.close
-			end.to_not raise_error
-		end
-	end
-	
-	describe "outside reactor" do
-		it "should fetch page" do
-			expect do
-				response = HTTP.get('https://www.google.com', wrappers)
-				response.connection.close
-			end.to_not raise_error
+module Async
+	module IO
+		module Wrap
+			module TCPSocket
+				def self.open(*args)
+					if Async::Task.current?
+						Async::IO::TCPSocket.new(*args)
+					else
+						::TCPSocket.open(*args)
+					end
+				end
+			end
 		end
 	end
 end
