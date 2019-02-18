bb6f3240dcebab448eb2cc2ff1893ad714e8116e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-18 16:58:00 +1300

    Update chat client to gracefully handle disconnections. Fixes #20.

--- examples/chat/client.rb
+++ examples/chat/client.rb
@@ -2,7 +2,8 @@
 
 $LOAD_PATH << File.expand_path("../../lib", __dir__)
 
-require 'async/reactor'
+require 'async'
+require 'async/notification'
 require 'async/io/stream'
 require 'async/io/host_endpoint'
 require 'async/io/protocol/line'
@@ -18,23 +19,39 @@ input = Async::IO::Protocol::Line.new(
 	)
 )
 
-Async::Reactor.run do |task|
+Async do |task|
 	socket = endpoint.connect
 	stream = Async::IO::Stream.new(socket)
 	user = User.new(stream)
 	
-	connection = task.async do
+	# This is used to track whether either reading from stdin failed or reading from network failed.
+	finished = Async::Notification.new
+	
+	# Read lines from stdin and write to network.
+	terminal = task.async do
+		while line = input.read_line
+			user.write_lines line
+		end
+	rescue EOFError
+		# It's okay, we are disconnecting, because stdin has closed.
+	ensure
+		finished.signal
+	end
+	
+	# Read lines from network and write to stdout.
+	network = task.async do
 		while line = user.read_line
 			puts line
 		end
+	ensure
+		finished.signal
 	end
 	
-	while line = input.read_line
-		user.write_lines line
-	end
-rescue EOFError
-	# It's okay, we are disconnecting, because stdin has closed.
+	# Wait for any of the above processes to finish:
+	finished.wait
 ensure
-	connection.stop
-	user.close
+	# Stop all the nested tasks if we are exiting:
+	network.stop if network
+	terminal.stop if terminal
+	user.close if user
 end
