a2d25c55a12337263c6d4eaea68caf2dfb0047c9
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-04-25 23:02:39 +1200

    Add udp client/server example, fixes #25.

---
+++ examples/udp/client.rb
@@ -0,0 +1,12 @@
+
+require 'async'
+require 'async/io'
+
+endpoint = Async::IO::Endpoint.udp("localhost", 5678)
+
+Async do |task|
+	endpoint.connect do |socket|
+		socket.send("Hello World")
+		pp socket.recv(1024)
+	end
+end
---
+++ examples/udp/server.rb
@@ -0,0 +1,14 @@
+
+require 'async'
+require 'async/io'
+
+endpoint = Async::IO::Endpoint.udp("localhost", 5678)
+
+Async do |task|
+	endpoint.bind do |socket|
+		while true
+			data, address = socket.recvfrom(1024)
+			socket.send(data.reverse, 0, address)
+		end
+	end
+end
