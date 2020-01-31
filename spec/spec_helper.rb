e4125ab7472323744df0dc64462e6ec1173dc870
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-01-31 22:42:33 +1300

    Remove rspec formatter hack.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,6 @@ require "async/rspec"
 
 require_relative 'addrinfo'
 
-class RSpec::Core::Formatters::DocumentationFormatter
-	def message(notification)
-		output.puts "#{current_indentation}#{notification.message}"
-	end
-end
-
 RSpec.configure do |config|
 	# Enable flags like --only-failures and --next-failure
 	config.example_status_persistence_file_path = ".rspec_status"
