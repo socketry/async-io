8358eea34be31c13da57d29bb9865941e1f716fb
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-04-23 15:22:59 +1200

    Add support for asynchronous thread pool.

---
+++ lib/async/io/threads.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+# Copyright, 2020, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require_relative 'notification'
+
+module Async
+	module IO
+		class Threads
+			def initialize(parent: nil)
+				@parent = parent
+			end
+			
+			def async(parent: (@parent or Task.current))
+				parent.async do |task|
+					notification = Async::IO::Notification.new
+					
+					thread = Thread.new do
+						yield
+					ensure
+						notification.signal
+					end
+					
+					task.annotate "Waiting for thread to finish..."
+					
+					notification.wait
+					
+					thread.value
+				ensure
+					if thread&.alive?
+						thread.raise(Stop)
+						
+						begin
+							thread.join
+						rescue Stop
+							# Ignore.
+						end
+					end
+					
+					notification&.close
+				end
+			end
+		end
+	end
+end
---
+++ spec/async/io/threads_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# Copyright, 2020, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'async/io/threads'
+
+RSpec.describe Async::IO::Threads do
+	include_context Async::RSpec::Reactor
+	
+	describe '#async' do
+		it "can schedule work on a different thread" do
+			thread = subject.async do
+				Thread.current
+			end.wait
+			
+			expect(thread).to be_kind_of Thread
+			expect(thread).to_not be Thread.current
+		end
+		
+		it "can kill thread when stopping task" do
+			sleeping = Async::IO::Notification.new
+			
+			thread = nil
+			
+			task = Async do
+				subject.async do
+					thread = Thread.current
+					sleeping.signal
+					sleep
+				end
+			end
+			
+			sleeping.wait
+			
+			task.stop
+			expect(thread.status).to be_nil
+		ensure
+			sleeping.close
+		end
+	end
+end
