8a3b9da431b7c9b40994bb2a2c2decaf199a07e4
Olle Jonsson <olle.jonsson@auctionet.com>
2021-06-08 15:20:48 +0200

    gems.rb: Conditionally install bake-modernize (not on JRuby).

--- gems.rb
+++ gems.rb
@@ -2,12 +2,22 @@
 
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in async-io.gemspec
 gemspec
+
+group :development do
+	gem "async-container", "~> 0.15"
+	gem "async-rspec", "~> 1.10"
+	gem "bake"
+	gem "bake-bundler"
+	gem "bake-modernize" unless defined?(JRUBY_VERSION)
+	gem "bundler"
+	gem "covered"
+	gem "rspec", "~> 3.0"
+end
 
 group :test do
 	gem 'benchmark-ips'
 	gem 'ruby-prof', platforms: :mri
-	
+
 	gem 'http'
 end
